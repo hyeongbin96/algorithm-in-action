ec55cbf434a608ef1c97663d09479520e1a7cc52
hyeongbin Jeon <gudqls1539@gmail.com>
2024-02-23 02:08:02 +0900

    [Bronze II] Title: 나머지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -0,0 +1,11 @@
+# 입력 받은 수를 42로 나눈 나머지들을 리스트에 넣고 중복을 제거한 후 리스트의 카운트를 출력하면 될듯
+# set() 함수는 리스트의 고유값을 집합으로 반환
+import sys
+
+result = []
+
+for i in range(1, 11) :
+    number = (int(input()) % 42)
+    result.append(number)
+
+print(len(set(result)))
