dfe3eaf8117a8822cb0e0593d23757f219acfff6
hyeongbin <gudqls1539@gmnail.com>
2024-02-23 01:27:36 +0900

    update repository

--- 백준/Bronze/10813. 공 바꾸기/10813. 공 바꾸기.py
+++
@@ -1,9 +0,0 @@
-n, m = map(int, input().split())
-
-basket = [i for i in range(1, n+1)]
-
-for i in range(m) :
-    i, j = map(int, input().split())
-    basket[i-1], basket[j-1] = basket[j-1], basket[i-1]
-
-print(*basket)
