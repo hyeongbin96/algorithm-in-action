17379a32d7e38fdda57beffdb935c877ec3d7751
hyeongbin Jeon <gudqls1539@gmail.com>
2024-02-23 13:53:25 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -9,3 +9,14 @@ for i in range(T) :
     for j in S :
         P += j*count
     print(P)
+
+# 다른 풀이자의 답
+# > 더 효율적이고 간결한거 같음
+    
+# T = int(input())
+
+# for i in range(T) :
+#     R, S = input().split()
+#     for j in range(len(S)) :
+#         print(S[j]*int(R), end='')
+#     print()
