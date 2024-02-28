e469cc90b99c18d3a917888d0ee6f3ff4de77534
hyeongbin Jeon <gudqls1539@gmail.com>
2024-02-28 22:03:52 +0900

    [Bronze I] Title: 진법 변환 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -5,15 +5,15 @@ N, B = map(int, input().split())
 result = []
 
 while True :
-    if N >= B :
-        remainder = (N % B)
-        result.append(remainder)
+    if N >= B :     # while을 도는동나 N을 B로 나눈 몫이 N 값이 된다.
+        remainder = (N % B)         
+        result.append(remainder)     
         N = (N // B)
-    else :
+    else :          # N을 B로 나눠가면서 B보다 N이 작아지면 종료하고 이전에 나눠진 몫을 리스트에 추가
         result.append(N)
         break
 
-result.reverse()
+result.reverse() # 가장 마지막 몫과 나머지 순으로 진법 변환을 해야되므로 리스트를 역순으로 변경
 
 for i in result :
     print(alpha[i], end='')
