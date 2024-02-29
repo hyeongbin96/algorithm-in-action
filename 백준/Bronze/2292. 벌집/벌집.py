b364e8b0f1b507dcce3bd4521de19e102b389a00
hyeongbin Jeon <gudqls1539@gmail.com>
2024-02-29 16:15:38 +0900

    [Bronze II] Title: 벌집, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,12 +1,12 @@
 N = int(input())
 
-count = 1
-sum = 1
+count = 1   # 시작하는 방도 지나간 방의 개수에 포함하니 무조건 1번방은 포함되므로 초기값은 1
+sum = 1     # 이웃하는 방의 개수. 이웃하는 방은 6의 배수를 더한만큼 늘어남 1, 7, 19, 37 ...
 
-if N == 1:
+if N == 1:      
     count = 1
 else :
-    while N > sum :
+    while N > sum :     # 입력 값이 방의 범위를 벗어나면 종료
         sum += (6*count)
         count += 1
 
