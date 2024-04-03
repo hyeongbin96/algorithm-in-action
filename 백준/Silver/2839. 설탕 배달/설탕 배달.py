73f54881bdf40c3350485c8351729df3630398bf
hyeongbin Jeon <gudqls1539@gmail.com>
2024-04-03 23:56:12 +0900

    Update 설탕 배달.py
    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,14 +1,17 @@
-N = int(input())
+N = int(input())    # 배달할 설탕 kg
 
-count = 0
+count = N // 5  # 배달할 설탕에서 최대로 가질 수 있는 최초의 5kg 개수
 
-while True :
-    if N % 5 == 0 :
-        count += N // 5
-        print(count)
-        break
-    N -= 3
-    count += 1
-    if N < 0 :
+if N % 5 == 0 : # 5의 배수인 경우 바로 출력
+    print(N // 5)
+else :  # 5의 배수가 아닌 5kg, 3kg 봉지의 개수 조합을 구해야 하는 경우
+    while count >= 0 : # 챙길 수 있는 5kg 봉지가 하나도 없어질 때 까지 수행 (0개도 수행)
+        sugar = N - (5 * count) # 5kg 봉지를 챙겼을 때 남은 설탕 무게
+        if sugar % 3 == 0 : # 5키로 봉지를 챙기고 3kg로 채워지면 각각의 개수 출력 후 종료
+            result = count + (sugar // 3)   
+            print(result)
+            break
+        else :  # 5키로 봉투를 제외한 남은 설탕 무게가 3kg로 채울 수 없으면 5kg 봉지의 개수를 하나 줄임
+            count -= 1
+    else :
         print(-1)
-        break
