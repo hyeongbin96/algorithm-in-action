1616b4739c009f5ec87e32a12f12a7a19d82cb0f
hyeongbin Jeon <gudqls1539@gmail.com>
2024-05-26 18:29:43 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,4 +1,3 @@
 def solution(s):
     answer = [int(i) for i in list(s.split())]
-    result = str(min(answer)) + ' ' + str(max(answer))
-    return result
+    return str(min(answer)) + ' ' + str(max(answer))
