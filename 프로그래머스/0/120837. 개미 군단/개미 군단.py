ad476071f33f088ac73ffab4959e25fe23d178a5
hyeongbin <gudqls1539@gmail.com>
2024-07-17 23:35:10 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,20 +1,3 @@
 def solution(hp):
-    answer = 0
-
-    if hp % 5 == 0:
-        answer = hp // 5
-    else:
-        if hp < 5:
-            if hp >= 3:
-                answer = (hp // 3) + (hp - 3)
-            else:
-                answer = hp
-        else:
-            if (hp % 5) < 3:
-                answer = (hp // 5) + (hp - (hp // 5) * 5)
-            elif (hp % 5) == 3:
-                answer = (hp // 5) + 1
-            else:
-                answer = ((hp // 5) + ((hp - (hp // 5) * 5) // 3) + (hp - (hp // 5) * 5) % 3)
-
+    answer = (hp // 5) + ((hp % 5) // 3) + ((hp % 5) % 3)
     return answer
