6f7d7ac9d028ff7c240ed55785668d3f6d826851
hyeongbin <gudqls1539@gmail.com>
2024-07-17 22:18:36 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -0,0 +1,18 @@
+def solution(age):
+    alpha = {
+        "0": "a",
+        "1": "b",
+        "2": "c",
+        "3": "d",
+        "4": "e",
+        "5": "f",
+        "6": "g",
+        "7": "h",
+        "8": "i",
+        "9": "j",
+    }
+    answer = ''
+    for i in str(age):
+        if i in alpha:
+            answer += alpha[i]
+    return answer
