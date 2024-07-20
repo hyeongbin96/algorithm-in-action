a2647171447e0520645ddd33c60825707cbc00b1
hyeongbin <gudqls1539@gmail.com>
2024-07-21 00:36:08 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,7 +1,2 @@
 def solution(my_string):
-    
-    for i in my_string:
-        if i in ("a", "e", "i", "o", "u"):
-            my_string = my_string.replace(i, "")
-
-    return my_string
+    return "".join([i for i in my_string if i not in "aeiou"])
