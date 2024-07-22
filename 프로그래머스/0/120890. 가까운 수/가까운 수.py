bfcd808cdf396ef4d30c814b5075685671803420
hyeongbin <gudqls1539@gmail.com>
2024-07-22 23:42:23 +0900

    [level 0] Title: 가까운 수, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -3,4 +3,4 @@ def solution(array, n):
     answer = []
     answer = [abs(n - i) for i in array]
             
-    return array[answer.index(min([abs(n - i) for i in array]))]
+    return array[answer.index(min(answer))]
