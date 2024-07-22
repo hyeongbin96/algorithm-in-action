402a3fff7953028517c299ed1539d997b7956d0b
hyeongbin <gudqls1539@gmail.com>
2024-07-23 00:07:56 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,11 +1,2 @@
 def solution(my_string):
-
-    answer = ""
-
-    for i in my_string:
-        if i.isupper():
-            answer += i.lower()
-        else:
-            answer += i.upper()
-
-    return answer
+    return "".join(i.lower() if i.isupper() else i.upper() for i in my_string)
