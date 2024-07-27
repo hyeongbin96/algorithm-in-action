de9747987efd66d5fc17d6180d030ceadd73eca9
hyeongbin <gudqls1539@gmail.com>
2024-07-28 01:43:37 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.33 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,7 +1,2 @@
 def solution(numbers):
-    answer = []
-    for idx, v in enumerate(numbers):
-        for j in numbers[idx + 1 : len(numbers) + 1]:
-            answer.append(v * j)
-
-    return max(answer)
+    return max((v * j) for idx, v in enumerate(numbers) for j in numbers[idx + 1 : len(numbers) + 1])
