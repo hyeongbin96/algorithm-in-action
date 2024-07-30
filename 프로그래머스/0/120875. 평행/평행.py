6fdbcec8de05dc9139d9ef40a3b3d9cea5983837
hyeongbin <gudqls1539@gmail.com>
2024-07-30 22:44:24 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -1,21 +1,12 @@
-def is_parallel(a, b, c, d):
-    x, y = 0, 1
-    g1 = (a[y] - b[y]) / (a[x] - b[x])
-    g2 = (c[y] - d[y]) / (c[x] - d[x])
-
-    return 1 if g1 == g2 else 0
-
-
 def solution(dots):
-    dots.sort(key=lambda x: x[0])
-    answer = 0
+    answer = []
+    dots.sort()
     a, b, c, d = dots
 
-    aa = is_parallel(a, b, c, d)
-    bb = is_parallel(a, c, b, d)
-    cc = is_parallel(a, d, b, c)
+    for i in range(len(dots)):
+        g1 = (a[0] - b[0]) / (a[1] - b[1])
+        g2 = (c[0] - d[0]) / (c[1] - d[1])
+        if g1 == g2:
+            answer.append(1)
 
-    if 1 in [aa, bb, cc]:
-        answer = 1
-
-    return answer
+    return 1 if 1 in answer else 0
