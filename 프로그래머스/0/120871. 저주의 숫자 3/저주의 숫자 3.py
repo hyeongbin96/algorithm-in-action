0716483d048de58c230bb826fd8723a5cc0e266d
hyeongbin <gudqls1539@gmail.com>
2024-07-30 14:19:30 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,13 +1,8 @@
 def solution(n):
-    answer = 1
-    while n > 0:
-
-        if answer % 3 == 0 or "3" in str(answer):
-            answer += 1
-            continue
-        else:
+    answer = 0
+    for i in range(1, n+1) :
+        answer +=1 
+        while answer % 3 == 0 or "3" in str(answer):
             answer += 1
 
-        n -= 1
-
-    return answer - 1
+    return answer
