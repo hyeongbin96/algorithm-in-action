c99ae210a30eb18c428947736bb4abb889c588c7
hyeongbin <gudqls1539@gmail.com>
2024-08-01 22:08:56 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,3 +1,12 @@
 def solution(id_pw, db):
-    answer = ''
+    answer = "fail"
+
+    for user in db:
+        if id_pw[0] == user[0]:
+            if id_pw[1] == user[1]:
+                answer = "login"
+            else:
+                answer = "wrong pw"
+            break
+
     return answer
