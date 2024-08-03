27d38bec9ad5fc78612ac83703e954a877649ab8
hyeongbin <gudqls1539@gmail.com>
2024-08-03 16:07:56 +0900

    [level 0] Title: k의 개수, Time: 42.74 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -1,6 +1,2 @@
 def solution(i, j, k):
-    answer = 0
-    for i in range(i, j + 1):
-        if str(k) in str(i):
-            answer += str(i).count(str(k))
-    return answer
+    return sum(str(i).count(str(k)) for i in range(i, j + 1) if str(k) in str(i))
