bb3dd3ddf1e4c9d3faeda76c5aa93797490420d2
hyeongbin <gudqls1539@gmail.com>
2024-08-03 16:40:59 +0900

    [level 0] Title: 문자열 밀기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,13 +1,10 @@
 def solution(A, B):
-    answer = 0
+    answer = -1
     A = [i for i in A]
     B = [i for i in B]
 
     for i in range(len(A)):
         if A == B:
-            break
-        else:
-            A[0], A[1:] = A[-1], A[0:-1]
-            answer += 1
-
-    return -1 if answer == len(A) else answer
+            return i
+        A[0], A[1:] = A[-1], A[0:-1]
+    return answer
