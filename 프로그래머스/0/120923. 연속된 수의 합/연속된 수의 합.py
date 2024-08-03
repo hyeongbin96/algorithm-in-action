a6cb3b837d07f556e71bc067012dbde29d182369
hyeongbin <gudqls1539@gmail.com>
2024-08-03 17:51:10 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -0,0 +1,8 @@
+def solution(num, total):
+    answer = []
+    average = total // num
+
+    for i in range(average - ((num - 1) // 2), average + ((num + 2) // 2)):
+        answer.append(i)
+
+    return answer
