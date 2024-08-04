5df4be150f3e93d46dc388dd12c84adad4dde6d0
hyeongbin <gudqls1539@gmail.com>
2024-08-04 20:51:39 +0900

    [level 2] Title: 숫자의 표현, Time: 3.69 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,3 +1,16 @@
+# def solution(n):
+#     answer = 1
+#     for i in range(1, n // 2 + 1):
+#         sum = i
+#         for j in range(i + 1, n // 2 + 2):
+#             sum += j
+#             if sum == n:
+#                 print(f"{i} 부터 {j}까지의 합계 = {sum}")
+#                 answer += 1
+
+#     return answer
+
+
 def solution(n):
     answer = 1
     for i in range(1, n // 2 + 1):
@@ -10,3 +23,7 @@ def solution(n):
             i += 1
 
     return answer
+
+
+# print(solution(15))
+# print(solution(30))
