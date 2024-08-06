1176b63e4e9c308406b4c74880b698f1dc72f389
hyeongbin <gudqls1539@gmail.com>
2024-08-06 21:14:23 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,10 +1,3 @@
 def solution(a, b, c):
-    answer = [a, b, c]
-    sum = 0
-    if a != b != c:
-        sum = a + b + c
-    if answer.count(a) == 2 or answer.count(b) == 2 and answer.count(c) == 2:
-        sum = (a + b + c) * (a * a + b * b + c * c)
-    if a == b == c:
-        sum = (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
-    return sum
+    answer = 0
+    return answer
