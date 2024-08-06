46fb209ab52299c5b419582b8e6908fdd206e1c5
hyeongbin <gudqls1539@gmail.com>
2024-08-06 21:28:23 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,8 +1,3 @@
 def solution(num_list):
-    answer = ["", ""]
-    for i in num_list:
-        if i % 2 == 0:
-            answer[0] += str(i)
-        else:
-            answer[1] += str(i)
-    return int(answer[0]) + int(answer[1])
+    answer = 0
+    return answer
