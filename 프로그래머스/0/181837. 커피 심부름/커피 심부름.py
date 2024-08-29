f9a10ab8910346c1b4b6fbf13a2d13c4f7784651
hyeongbin <gudqls1539@gmail.com>
2024-08-29 21:31:41 +0900

    [level 0] Title: 커피 심부름, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,11 @@
+def solution(order):
+    answer = 0
+    for menu in order:
+        if "americanoice" in menu:
+            answer += 4500
+        elif "cafelatte" in menu:
+            answer += 5000
+        else:
+            answer += 4500
+
+    return answer
