800e8d610443b2aa4044212dc7475ea3f769260d
hyeongbin <gudqls1539@gmail.com>
2024-09-23 22:56:11 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -2,11 +2,6 @@ def solution(food):
     answer = ""
 
     for i in range(1, len(food)):
-        if food[i] < 2:
-            continue
-        elif food[i] % 2 == 0:
-            answer += str(i) * (food[i] // 2)
-        else:
-            answer += str(i) * (food[i] // 2)
-            
+        answer += str(i) * (food[i] // 2)
+
     return answer + "0" + "".join(list(reversed(answer)))
